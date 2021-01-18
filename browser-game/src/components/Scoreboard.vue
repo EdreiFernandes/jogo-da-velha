b2f0a59e08fb1e4ddef1e99792929fb2d51ca2c9
Edrei <edreifernandes@gmail.com>
2021-01-18 02:49:58 -0300

    Criando componente para mostrar o placar

---
+++ browser-game/src/components/Scoreboard.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="scoreboard" class="row col-sm-7 my-4 text-center">
+        <div id="player-1" class="col-5">
+            <h4>Jogador 1</h4>
+            <h1>{{ player[0].victories }}</h1>
+            <h6>vitórias</h6>
+        </div>
+
+        <h2 class="col-2 mt-5"><b>X</b></h2>
+
+        <div id="player-2" class="col-5">
+            <h4>Jogador 2</h4>
+            <h1>{{ player[1].victories }}</h1>
+            <h6>vitórias</h6>
+        </div>
+
+        <div class="d-flex col-12 mt-4 justify-content-between">
+            <button type="button" class="btn btn-outline-danger col-5" @click="end()">Encerrar</button>
+            <button type="button" class="btn btn-outline-dark col-5" @click="restart()">Reiniciar</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            player: {
+                type: Array
+            },
+        },
+        methods: {
+            end() {
+                this.$emit("End");
+            },
+            restart(){
+                this.$emit("Restart");
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>

7ab8b929b67b78118835e343ceb44895d2f604dc
Edrei <edreifernandes@gmail.com>
2021-01-18 02:49:31 -0300

    Criando componente para mostrar o resultado

---
+++ browser-game/src/components/Result.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="result" class="row col-sm-7 my-5 justify-content-between">
+        <h2 class="col-10 text-center">{{ result }}</h2>
+
+        <button type="buttom" class="btn btn-warning col-1" @click="close()">
+            <i class="fas fa-times"></i>
+        </button>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            result: {
+                type: String,
+                default: ""
+            },
+        },
+        methods: {
+            close() {
+                this.$emit("Close");
+            }
+        },
+    }
+</script>
